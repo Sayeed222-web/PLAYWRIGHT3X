cc9dee45aa44bb6578655ce3c374de6b6c994d2d
sayeed khan <durranisayeedkhan8@gmail.com>
2026-07-10 09:38:17 +0530

    second commit

---
+++ Chapter_01_Basics/Testfile.js
@@ -0,0 +1,9 @@
+let userAge = 19;
+
+if (userAge >= 21) {
+    console.log("You can enter the lounge.");
+} else if (userAge >= 18) {
+    console.log("You can enter, but you get a wristband.");
+} else {
+    console.log("Access denied. Too young!");
+}
